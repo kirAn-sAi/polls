0023969c2eaa791cba5a379b2b8fbe8e532e4f73
Kiran Palika <kiran.palika@oracle.com>
2023-10-03 00:03:19 +0530

    Added views and their respective templeates to display data and do the appropriate minimal actions

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:q_id>/", views.detail, name="detail"),
+    path("<int:q_id>/result/", views.result, name="result"),
+    path("<int:q_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import Http404
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 
 
 def index(request):
-    return HttpResponse("Hi Kiran, we are up now!")
+    # template = loader.get_template("polls/index.html")
+    context = {"latest_questions_list": Question.objects.all()}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, q_id):
+    qn = get_object_or_404(Question, pk=q_id)
+    return render(request, "polls/detail.html", {"question": qn})
+
+
+def result(request, q_id):
+    question = get_object_or_404(Question, pk=q_id)
+    return render(request, "polls/result.html", {"question": question})
+
+
+def vote(request, q_id):
+    question = get_object_or_404(Question, pk=q_id)
+    try:
+        choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice",
+            }
+        )
+    else:
+        choice.votes += 1
+        choice.save()
+    return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
+
